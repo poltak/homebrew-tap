8ac5ad89ce7f6fb32c7f079497f2f01a3b355a79
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2026-02-18 22:14:22 +0700

    middleclick-poltak v0.1.2

--- Casks/middleclick-poltak.rb
+++ Casks/middleclick-poltak.rb
@@ -1,8 +1,8 @@
 cask "middleclick-poltak" do
-  version "0.1.1"
-  sha256 "7a5618e4009d5941be6bbcefd770e730a9bbd1cec05d1d66e378ed1b49a4623d"
+  version "0.1.2"
+  sha256 "c162331654170691f0f621f6e3e9a84adda440a668a478f524ac86ff16049775"
 
-  url "https://github.com/poltak/middleclick/releases/download/v0.1.1/MiddleClick.app.zip",
+  url "https://github.com/poltak/middleclick/releases/download/v0.1.2/MiddleClick.app.zip",
       verified: "github.com/poltak/"
   name "MiddleClick"
   desc "Three-finger trackpad click/tap to middle click remapper"
