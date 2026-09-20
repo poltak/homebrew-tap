f131670a752018f7ea2050718109b793a5faee70
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2026-09-20 22:37:59 +0700

    Remove deprecated URL verification

--- Casks/middleclick-poltak.rb
+++ Casks/middleclick-poltak.rb
@@ -2,8 +2,7 @@ cask "middleclick-poltak" do
   version "0.2.0"
   sha256 "a314b2c91f7244b96146ba7ecab278b5c1eef08fdb9611a6672246a12a59cb03"
 
-  url "https://github.com/poltak/middleclick/releases/download/v0.2.0/MiddleClick.app.zip",
-      verified: "github.com/poltak/"
+  url "https://github.com/poltak/middleclick/releases/download/v0.2.0/MiddleClick.app.zip"
   name "MiddleClick"
   desc "Three-finger trackpad click/tap to middle click remapper"
   homepage "https://github.com/poltak/middleclick"
